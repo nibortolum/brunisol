e58f8d2448c889469a746ed47995a87dcb2cbe18
nibortolum <matthieu.mulot@gmail.com>
2019-08-20 13:34:47 +0200

    add ski function

---
+++ R/ski.R
@@ -0,0 +1,62 @@
+#' Ski : Draw a Brunii Morphotype
+#'
+#' @return print a Brunii morphotype
+#' @export
+#'
+#' @examples
+#' ski()
+ski <- function(){
+  cat("
+      -----------------------------------/+yhhhhhhhhdmmmmmmmmmmmmmmmmmmmddy+++/:--------------------------
+      ---------------------------------/oyyyyhhhyhhhhddmmmmNNNNNNNNNNNNNNNmhys+////:----------------------
+      -------------------------------/+syyhhyyysyhdmmmmNNNNNNNNNNNNNNNNmmmmdddhs+/://:--------------------
+      -----------------------------:ossssyssssyhddmmmmNNNNNNNNNNNNNNNNNNNmmmmmmmhso/:/:-------------------
+      ---------------------------:+yyhyyssssyhhdddmmmmmNNmmmNNNNNNNNNNNNNNNNNNNNmmds+//-------------------
+      --------------------------:shhhhyyyyhhhhhhhdddmmmNNNNNNNNNNNNNNNNNNNNNNNNNmmmdyo/-------------------
+      --------------------------ohhhyyyyhyyyyyhhhdddmmmmmNNNNNNNNNMMMMMNNMNNNNMMNmmmdy+-------------------
+      -------------------------/hdhhhhhyysyyyyhhhdmmmmNNNNNNNNNNNNNMMNNNNNNNMMMMNNmmmdo:------------------
+      -------------------------+hhhdddhhhyyyhhddmmmNNNNNNNNNNNNNNNNNNNNNMMMMMMMMNNNNmmh:------------------
+      -------------------------+hdmdhddddddddddmmmmmmmmmNNmmmmdddmmmmNNNNNNMMMMNNNNNNmmo------------------
+      -------------------------sdddhhhhhhhdhddddddddddddddddddmmmmNNNNNNNNNNMMNNNNNNNmmy:-----------------
+      ------------------------:sddhhhhhdddddmmmmmddhhhhyssooooosyyhdmNNNNNNNMNNNNNNNNNmd/-----------------
+      ------------------------:odddddddmmmmmmmddhhyyso++///////+++ooshdmmmmNNNNNNNNNmmmms/----------------
+      -----------------------:/yddddmmmmNNNmdsoo++/:::::::::////++++oosssyyyhhdmNNNNNmmm+/::--------------
+      -----------------------:+hddmNNNNNNmmh/----------::::://///+++ooosssssyyymNNNMNmmm/:::--------------
+      ------------------------/sdmmmmmmmdhy/:+oooo++/:://:::////++++oossssssyyydNNNMNNmm/-----------------
+      ------------------------:+hdmmmmmdyo+/+osyhddddys+//://+++oosyhddhddhhyyyhNNNMNNmh/-----------------
+      -------------------------/hmmmddddhhhsosssyyyhhhdhs+//+ossydmmmmmmmmddddddNNNNNNms:-----------------
+      -------------------------:smmNNNmhmd+/+sssyhhhhyyyhdhdmmmmmddhddhhhyyyyydmmmNdNNd/------------------
+      --------------------------+hmdNNs:d++ossysmmNdhhso+mho+sdNdddddmmNmmdhhyyymNNNNNo-------------------
+      --------------------------oddsmh/.y/-://+osyyyss+/:do-/ohNdddddmmmdhmdhyysdNNhNm/-------------------
+      --------------------------:+/sdo-.:y-.--:/++ooo+::o+.-/oymdhhhhhhyyyyyyyyshdNhNh:-------------------
+      ----------------------------:+s/...-o+:::::///::+s/..-/oyyddhyyyyyyyyyssyhshdhh/--------------------
+      -----------------------------:-:-....-+ooooooosys:...-/osyydddhyssssyyhhysshhy/------------::-------
+      -----------------------------.----......-://++//:--..-/osyyyyyyhhhhyysssssohy+------------::::::::--
+      ----------------------------.....-....----:::///-....:+osyyyyyssssosssssssoyo:------------::::::::::
+      ::---------------------------..-----------:////:/+o/:/+syyhyyyyssoooosssssss+---------------::::::::
+      ::----------------------------::-.------::://::-:ooo+osyhddhhyyyssoosssssoss/-----------------::::::
+      ---------------------------------.-----:::::::---:::/ososyyyyyyyssssssssso::------------------::::::
+      -----:---------------------------..----::/++/:::::::/++++ssssyyyyyyyyssss+----:::-----------::::::::
+      ::::::----------------------------.-----:+sys++//+++ossssyyyyyyhdhyssssss/----:::---------::::::::::
+      :::::::---------------------------------:/+/:://+++osyyhhhhhhhhhhysssssso:----------------::::::::::
+      ::::::::--------------------------------:::-----::/+osssyyyyyyyyyyssssss/----::::::-------::::::::::
+      ::::::::::--------------------------------------::/+oosssssssssssssysss/::::::::::::::--::::::::::::
+      :::::::::::----------------------:::-------------::///+ooossssssssssso::::::::::::::::::::::::::::::
+      :::::::::--------::------------:::::-.--:----------://++oossssssssyso:---::::::::::::::---::::::::::
+      ::::----------:-:------------:::::::-..---:::---::://+++osssssssssyyo:---::::::::::::::-----::::::::
+      ::-----------::::-----------::::::::-..-----::://+++ooooossssyyyyyyys::-::::::::::::::::::::::::::::
+      ::----::::--::::::-------::::::::---...-------:/++osssssssyyyyyyyysyso//::::::::::::::::::::::::::::
+      :::::::::::::::::::::::::::------......-------:::/+oosssyyyysyyysssyyyyysso+//::////::::::::::::::::
+      :::::::::::::::::::::::--------.......---------:::/++oosssssssssssyyyyyyyhhyyyssssyysso//:::::::::::
+      ::::::::::-:-+s+//:---.----------.....------:::::///++ooossssssssssyyyyyyyyyyyyyyyyyyhhhysoo++//::::
+      :::--------://o/:-------:::::::---....------:::::://++oooooosssssssyyyyyyyyyyyyyyyyyyyhhhhyyssssooo+
+      -----:::/://o//o+//::::::::::::---...-----:::::::////++oooooosssssssyyyyyyyyyyyyyyyyyhhhdhyyyyyyyyyy
+      --::/+o+o+oo+s/os+/::////////::---..-----::::::://///++oooooossssossssyyyyyyyyyyyyyyyhhhhhyyyyyyyyyy
+      -:://osoooooo+s+ss+:::://////::----...---:::::::///++++ooooosssooooosssssssssyyssssyhhhdhyyyyyyyyyyy
+      :////oys+sysooosooso/:--::////::---.....--:::--://+++++++++oooo++++oooossoooooooosyyhhdhyyyyyyyyyyyy
+      /++//oyy+ohhoossyosys/:--:::::::::--------::::-::/+++++++//++++/////+++++///++++osyhhhhyyyyyyyyyyyss
+      ++o+/+yho+yhyoyysysoyyo/::::::::///:::::::://::-://++////:::///:::://///////++oooyyhhhyyyssssssyysss
+      oooo+/yhs+sdhysyhsssoyhyo+////////////////////::::////::::::::::////////+++oososyyhhhyyssssssssyyssy
+      ossss/yhy+shhyshhyooyssyhyo++++++++++++///////:::::/:::::::::///////+++++ooooosshhsyhysssssssosysosh
+      ")
+}
